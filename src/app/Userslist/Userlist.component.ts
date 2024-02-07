1b3df785779465ed022d1ee58bc86989ea7fb201
shubhammohite <shubhammohite9081@gmail.com>
2024-02-08 00:28:57 +0530

    Perform CRUD Operation by using db.json file

---
+++ src/app/Userslist/Userlist.component.ts
@@ -0,0 +1,71 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { CoreService } from '../core/core.service';
+import { UsersService } from '../services/users.service';
+
+@Component({
+  selector: 'app-emp-add-edit',
+  templateUrl: './Userlist.component.html',
+  styleUrls: ['./Userlist.component.scss'],
+})
+export class UserlistComponent implements OnInit {
+  empForm: FormGroup;
+
+  education: string[] = [
+    'Matric',
+    'Diploma',
+    'Intermediate',
+    'Graduate',
+    'Post Graduate',
+  ];
+
+  constructor(
+    private _fb: FormBuilder,
+    private Service: UsersService,
+    private _dialogRef: MatDialogRef<UserlistComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private _coreService: CoreService
+  ) {
+    this.empForm = this._fb.group({
+      firstName: '',
+      lastName: '',
+      email: '',
+      gender: '',
+      education: '',
+      company: '',
+    });
+  }
+
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data);
+  }
+
+  onFormSubmit() {
+    if (this.empForm.valid) {
+      if (this.data) {
+        this.Service
+          .updateEmployee(this.data.id, this.empForm.value)
+          .subscribe({
+            next: (val: any) => {
+              this._coreService.openSnackBar('Employee detail updated!');
+              this._dialogRef.close(true);
+            },
+            error: (err: any) => {
+              console.error(err);
+            },
+          });
+      } else {
+        this.Service.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            this._coreService.openSnackBar('Employee added successfully');
+            this._dialogRef.close(true);
+          },
+          error: (err: any) => {
+            console.error(err);
+          },
+        });
+      }
+    }
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,95 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { UserlistComponent } from './Userslist/Userlist.component';
+import { UsersService } from './services/users.service';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { CoreService } from './core/core.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+
+    'firstName',
+    'lastName',
+    'email',
+    'gender',
+    'education',
+    'company',
+    'action',
+  ];
+  dataSource!: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private _dialog: MatDialog,
+    private Service: UsersService,
+    private _coreService: CoreService
+  ) {}
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  openAddEditEmpForm() {
+    const dialogRef = this._dialog.open(UserlistComponent);
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      },
+    });
+  }
+
+  getEmployeeList() {
+    this.Service.getEmployeeList().subscribe({
+      next: (res) => {
+        this.dataSource = new MatTableDataSource(res);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      },
+      error: console.log,
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  deleteEmployee(id: number) {
+    this.Service.deleteEmployee(id).subscribe({
+      next: (res) => {
+        this._coreService.openSnackBar('Employee deleted!', 'done');
+        this.getEmployeeList();
+      },
+      error: console.log,
+    });
+  }
+
+  openEditForm(data: any) {
+    const dialogRef = this._dialog.open(UserlistComponent, {
+      data,
+    });
+
+    dialogRef.afterClosed().subscribe({
+      next: (val) => {
+        if (val) {
+          this.getEmployeeList();
+        }
+      },
+    });
+  }
+}
